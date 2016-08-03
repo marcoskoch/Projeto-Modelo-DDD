68452d138a065023f889820cf7298d06e527abb5
Marcos Michel Machado Koch <marcos.koch@cwinet.local>
2016-08-03 12:47:44 -0300

    inicial

---
+++ ProjetoModeloDDD.MVC/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.MVC.ViewModels;
+
+namespace ProjetoModeloDDD.MVC.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            CreateMap<ClienteViewModel, Cliente>();
+            CreateMap<ProdutoViewModel, Produto>();
+        }
+    }
+}
---
+++ ProjetoModeloDDD.MVC/Controllers/ClientesController.cs
@@ -0,0 +1,102 @@
+﻿using AutoMapper;
+using ProjetoModeloDDD.Application.Interface;
+using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.MVC.ViewModels;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace ProjetoModeloDDD.MVC.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private readonly IClienteAppService _clienteApp;
+
+        public ClientesController(IClienteAppService clienteApp)
+        {
+            _clienteApp = clienteApp;
+        }
+
+        public ActionResult Index()
+        {
+            var clienteViewModel = Mapper.Map<IEnumerable<Cliente>, IEnumerable<ClienteViewModel>>(_clienteApp.GetAll());
+
+            return View(clienteViewModel);
+        }
+
+        public ActionResult Especiais()
+        {
+            var clienteViewModel = Mapper.Map<IEnumerable<Cliente>, IEnumerable<ClienteViewModel>>(_clienteApp.ObterClientesEspeciais());
+
+            return View(clienteViewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var cliente = _clienteApp.GetById(id);
+            var clienteViewModel = Mapper.Map<Cliente, ClienteViewModel>(cliente);
+
+            return View(clienteViewModel);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ClienteViewModel cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                var clienteDomain = Mapper.Map<ClienteViewModel, Cliente>(cliente);
+                _clienteApp.Add(clienteDomain);
+
+                RedirectToAction("Index");
+            }
+
+            return View(cliente);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var cliente = _clienteApp.GetById(id);
+            var clienteViewModel = Mapper.Map<Cliente, ClienteViewModel>(cliente);
+
+            return View(clienteViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ClienteViewModel cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                var clienteDomain = Mapper.Map<ClienteViewModel, Cliente>(cliente);
+                _clienteApp.Update(clienteDomain);
+
+                RedirectToAction("Index");
+            }
+
+            return View(cliente);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var cliente = _clienteApp.GetById(id);
+            var clienteViewModel = Mapper.Map<Cliente, ClienteViewModel>(cliente);
+
+            return View(clienteViewModel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var cliente = _clienteApp.GetById(id);
+            _clienteApp.Remove(cliente);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
